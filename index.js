d91882b69c9e6913a9d535d81128a15694f9d2d8
Parshvi06 <true.living1106@gmail.com>
2024-08-07 20:25:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');     //importing express
+const UserRouter = require('./routers/userRouter');
+
+const app = express();             //initializing express
+
+const port = 5000;
+    
+//middleware
+app.use('/user',UserRouter);
+
+//route or endpoint
+app.get('/',(req,res)=>{
+    res.send('Response from express');
+});
+
+app.get('/add',(req,res)=>{
+    res.send('Response from add');
+});
+
+app.get('/getall',(req,res)=>{
+    res.send('Response from getall');
+});
+
+app.get('/update',(req,res)=>{
+    res.send('Response from update');
+});
+
+
+app.listen(port,()=>{console.log('server started')});
