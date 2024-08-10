6dc566693068ac5a7b15b8f5db3c9daf3fb05a2c
Parshvi06 <true.living1106@gmail.com>
2024-08-10 20:26:48 +0530

    DATABASE

---
+++ connection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const url = "mongodb+srv://trueliving1106:parshvi06@cluster0.cwdq4.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=cluster0"
+
+//connect to the database
+
+
+//asynchronous- returns a promise
+mongoose.connect(url)
+.then((result) => {
+    console.log('connected to database');
+    
+})
+.catch((err) => {
+    console.log(err);
+    
+});
